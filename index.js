33711643e60b3848b61a4a2287fe605dc2c0d435
Itsjustblack <jason.aghedo@gmail.com>
2022-07-30 11:04:37 +0100

    Update index.js
    FIxed: Doesnt allow NaN into input

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 // A Basic Arithmetic Calculator
-const operator = prompt('Enter Operator ( either +, -, * or / ):');
-
+   const operator = prompt('Enter Operator ( either +, -, * or / ):');
    const num1 = parseFloat(prompt('Enter the First number: '));
    const num2 = parseFloat(prompt('Enter the Second number: '));
+   let result = 0;
 
-let result = 0;
-
-if(typeof num1 !== 'number' || typeof num2 !== 'number') 
-   throw new Error('Invalid Number my');
-  try{ if (operator === '+') {
-      result = num1 + num2;
-      console.log(`${num1} ${operator} ${num2} = ${result}`);
-      alert(`${num1} ${operator} ${num2} = ${result}`);
-   }
-   else if (operator === '-') {
-      result = num1 - num2;
-      console.log(`${num1} ${operator} ${num2} = ${result}`);
-      alert(`${num1} ${operator} ${num2} = ${result}`);
-   }
-   else if (operator === '*') {
-      result = num1 * num2;
-      console.log(`${num1} ${operator} ${num2} = ${result}`);
-      alert(`${num1} ${operator} ${num2} = ${result}`);
-   }
-   else if(operator ===  '/'){
-      result = num1 / num2;
-      console.log(`${num1} ${operator} ${num2} = ${result}`);
-      alert(`${num1} ${operator} ${num2} = ${result}`);
-  }
-  else{
-  console.log(`Invalid Operator`);
-  alert(`Invalid Operator`);
-  }
+try{
+   if((isNaN(num1)) || (isNaN(num2))) 
+      throw new Error('Invalid Number');
+   else
+   if (operator === '+') {
+         result = num1 + num2;
+         console.log(`${num1} ${operator} ${num2} = ${result}`);
+         alert(`${num1} ${operator} ${num2} = ${result}`);
+      }
+      else if (operator === '-') {
+         result = num1 - num2;
+         console.log(`${num1} ${operator} ${num2} = ${result}`);
+         alert(`${num1} ${operator} ${num2} = ${result}`);
+      }
+      else if (operator === '*') {
+         result = num1 * num2;
+         console.log(`${num1} ${operator} ${num2} = ${result}`);
+         alert(`${num1} ${operator} ${num2} = ${result}`);
+      }
+      else if(operator ===  '/'){
+         result = num1 / num2;
+         console.log(`${num1} ${operator} ${num2} = ${result}`);
+         alert(`${num1} ${operator} ${num2} = ${result}`);
+     }
+     else{
+        console.log(`Invalid Operator`);
+        alert(`Invalid Operator`);
+      }
 }
 catch (e) {
    console.log(e);
